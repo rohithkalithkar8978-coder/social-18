bade5dd1bfe12c7e11eabc86ec177b33ba27d3bf
Rohith Kalithkar <rohithkalithkar8978@gmail.com>
2026-08-16 13:19:55 +0000

    Pass sports data to template

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)  # <--- Make sure this variable is named 'app'
+app = Flask(__name__)
+
+# Dictionary expected by templates/index.html
+sports_data = {
+    "cricket": "Cricket",
+    "football": "Football",
+    "badminton": "Badminton"
+}
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', sports=sports_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
